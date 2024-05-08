2436997302abd1981359f3cc900bdfdcffd3cd60
JLisenbee <jakelisenbee@gmail.com>
2024-05-08 19:20:17 -0400

    Imported Swing

--- ShipBuilder/src/home/Main.java
+++ ShipBuilder/src/home/Main.java
@@ -1,14 +1,30 @@
 package home;
+import javax.swing.*;
 
 public class Main {
 
 	public static void main(String[] args) {
 
-
-		Ship spotter = new Ship(ShipTypes.FRIGATE, "Queen Anne's Revenge");
+		JFrame frame = new JFrame("Ship Builder");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
+		JButton test = new JButton("Boop!");
+		// TODO Set Button Action
+		test.setAction(null);
+		
+		
+		frame.getContentPane().add(test);
+		
+		frame.pack();
+		frame.setVisible(true);
+		
+		
+		
+		Ship spotter = new Ship(ShipTypes.FRIGATE, "Queen Anne's Revenge");
 		spotter.addSystem(new Sys(SysList.THRUSTER_OVERCHARGERS));
 		spotter.addSystem(new Sys(SysList.SPITFIRE_CIWS));
+		
+		
 		
 		System.out.println(spotter.toString());
 
