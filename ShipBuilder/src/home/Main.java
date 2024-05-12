c2c1c23a522c1d3037436705aabc4e8ee53bb727
JLisenbee <jakelisenbee@gmail.com>
2024-05-11 23:16:34 -0400

    Testing

--- ShipBuilder/src/home/Main.java
+++ ShipBuilder/src/home/Main.java
@@ -22,15 +22,24 @@ public class Main {
 		
 		
 		Ship spotter = new Ship(ShipTypes.FRIGATE, "Queen Anne's Revenge");
+		
 		spotter.addSystem(new Sys(SysList.THRUSTER_OVERCHARGERS));
 		spotter.addSystem(new Sys(SysList.SPITFIRE_CIWS));
 		spotter.addSystem(new Sys(SysList.ADAPTIVE_PLATING));
 		
-		spotter.removeSystem(SysList.SPITFIRE_CIWS);
+		Ship battleship = new Ship(ShipTypes.FLAGSHIP, "OWC IOWA");
 		
-		
+		battleship.addSystem(new Sys(SysList.ARTILLERY));
+		battleship.addSystem(new Sys(SysList.TURRET_MOUNT));
+		battleship.addSystem(new Sys(SysList.ARTILLERY));
+		battleship.addSystem(new Sys(SysList.TURRET_MOUNT));
+		battleship.addSystem(new Sys(SysList.ARTILLERY));
+		battleship.addSystem(new Sys(SysList.TURRET_MOUNT));
+		battleship.addSystem(new Sys(SysList.SPITFIRE_CIWS));
+		battleship.addSystem(new Sys(SysList.ADAPTIVE_PLATING));
 		
 		System.out.println(spotter.toString());
+		System.out.println(battleship.toString());
 
 	}
 
