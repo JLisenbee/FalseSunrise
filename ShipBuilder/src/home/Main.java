c68ba672fd757525763eb35de51dd2fbb7a51014
JLisenbee <jakelisenbee@gmail.com>
2024-05-19 21:04:18 -0400

    created radio buttons

--- ShipBuilder/src/home/Main.java
+++ ShipBuilder/src/home/Main.java
@@ -1,14 +1,56 @@
 package home;
-import javafx.*;
 
-public class Main {
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.layout.*;
+import javafx.stage.Stage;
 
-	public static void main(String[] args) {
+public class Main extends Application {
+	
+	@Override
+	public void start(Stage mainstage) throws Exception {
 
+		mainstage.setTitle("False Sunrise Ship Builder");
 		
-		Frame root = new Frame();
+		GridPane root = new GridPane();
 		
-
+		root.setHgap(10);
+		root.setVgap(10);
+		
+		ToggleGroup shipTypes = new ToggleGroup();
+		RadioButton corvButton = new RadioButton("Corvette");
+		corvButton.setToggleGroup(shipTypes);
+		corvButton.setSelected(true);
+		RadioButton frigButton = new RadioButton("Frigate");
+		frigButton.setToggleGroup(shipTypes);
+		RadioButton destButton = new RadioButton("Destroyer");
+		destButton.setToggleGroup(shipTypes);
+		RadioButton cruiButton = new RadioButton("Cruiser");
+		cruiButton.setToggleGroup(shipTypes);
+		RadioButton flagButton = new RadioButton("Flagship");
+		flagButton.setToggleGroup(shipTypes);
+	
+		root.add(new Label("Title Space"), 0, 0);
+		
+		root.add(corvButton, 0, 1);
+		root.add(frigButton, 1, 1);
+		root.add(destButton, 2, 1);
+		root.add(cruiButton, 3, 1);
+		root.add(flagButton, 4, 1);
+		
+		
+		mainstage.setScene(new Scene(root));
+		mainstage.show();
+		
 	}
+
+	
 
 }
